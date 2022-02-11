f68fbddbd9f6a606660a76f24ea391eb1abb9c59
Wfect <85443236+Wfect@users.noreply.github.com>
2022-02-11 20:50:53 +0300

    Create AbstractAyarlar.java

---
+++ AbstractAyarlar.java
@@ -0,0 +1,9 @@
+
+public abstract class AbstractAyarlar {
+	
+	abstract void AyarlarıGüncelle();
+	abstract void AyarlarıAl();
+	abstract void AyarlarıGüncelle2();
+	abstract void AyarlarıGüncelle3();
+
+}
